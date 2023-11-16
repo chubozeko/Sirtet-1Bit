12ae23f7328b85da8fe3544f21687e3521d4508a
Chubo Zeko <chubozeko@gmail.com>
2023-11-16 14:39:20 +0200

    3. Added Line Clearing (from default Tetris)

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -53,6 +54,8 @@ public class Piece : MonoBehaviour
             {
                 transform.position -= new Vector3(0, -1, 0);
                 AddToGrid();
+                CheckForLines();
+
                 this.enabled = false;
                 // Only spawn a new piece if there is no other piece in the Spawner's position
                 if (transform.position != FindObjectOfType<PieceSpawner>().transform.position) 
@@ -72,12 +75,12 @@ public class Piece : MonoBehaviour
 
     bool ValidMove()
     {
-        foreach (Transform children in transform)
+        foreach (Transform childBlock in transform)
         {
-            if (children.CompareTag("PieceBlock")) 
+            if (childBlock.CompareTag("PieceBlock")) 
             {
-                int roundedX = Mathf.RoundToInt(children.transform.position.x);
-                int roundedY = Mathf.RoundToInt(children.transform.position.y);
+                int roundedX = Mathf.RoundToInt(childBlock.transform.position.x);
+                int roundedY = Mathf.RoundToInt(childBlock.transform.position.y);
 
                 if (roundedX < 0 || roundedX >= gridWidth || roundedY < 0 || roundedY >= gridHeight)
                 {
@@ -95,15 +98,65 @@ public class Piece : MonoBehaviour
 
     void AddToGrid()
     {
-        foreach (Transform children in transform)
+        foreach (Transform childBlock in transform)
         {
-            if (children.CompareTag("PieceBlock")) 
+            if (childBlock.CompareTag("PieceBlock")) 
             {
-                int roundedX = Mathf.RoundToInt(children.transform.position.x);
-                int roundedY = Mathf.RoundToInt(children.transform.position.y);
+                int roundedX = Mathf.RoundToInt(childBlock.transform.position.x);
+                int roundedY = Mathf.RoundToInt(childBlock.transform.position.y);
 
-                grid[roundedX, roundedY] = children;
+                grid[roundedX, roundedY] = childBlock;
             }
         }
     }
+
+    void CheckForLines()
+    {
+        for (int i = gridHeight-1; i >= 0; i--)
+        {
+            if (HasLine(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+            }
+        }
+    }
+
+    bool HasLine(int i)
+    {
+        for (int j = 0; j < gridWidth; j++)
+        {
+            if (grid[j,i] == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void DeleteLine(int i)
+    {
+        for (int j = 0; j < gridWidth; j++)
+        {
+            Destroy(grid[j,i].gameObject);
+            grid[j,i] = null;
+        }
+    }
+
+    void RowDown(int i)
+    {
+        for (int y = i; y < gridHeight; y++)
+        {
+            for (int j = 0; j < gridWidth; j++)
+            {
+                if (grid[j,y] != null)
+                {
+                    grid[j, y-1] = grid[j, y];
+                    grid[j, y] = null;
+                    grid[j, y-1].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
+        }
+    }
+
 }
